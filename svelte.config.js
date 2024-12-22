e6c8f9af24e30b1849b3b68894e152db821fc37d
raluvy95 <ralucadoge069@gmail.com>
2024-12-22 15:44:56 +0200

    updated something, hopefully it'll work

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,10 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '200.html',
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
